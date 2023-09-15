bc32022ee47712ef8b08aea2a7d880ee07c4c486
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-09-15 11:30:15 -0400

    include boilerplate code for brand colours/fonts in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
+// 1. Import the extendTheme function
+import { extendTheme } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    // 900: '#1a365d',
+    // 800: '#153e75',
+    // 700: '#2a69ac',
+  },
+}
+
+// 3. Pass the `theme` prop to the `ChakraProvider`
+
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
     </ChakraProvider>
   </React.StrictMode>,
 )
